60e16a1891efe94971540cf2bc3209963597da75
jiakjiak <88105937+jiakjiak@users.noreply.github.com>
2023-04-05 08:40:36 -0700

    Created index, navbar, header, default fonts,

---
+++ src/script.js
@@ -0,0 +1,24 @@
+//navbar animation//
+const navbarScrolling = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 50) {
+    navbarScrolling.classList.add("navbar-scrolled");
+  } else if (window.scrollY < 50) {
+    navbarScrolling.classList.remove("navbar-scrolled");
+  }
+});
+
+//banner animation//
+window.onload = function () {
+  var tl = new TimelineLite({ delay: 1 }),
+    firstBg = document.querySelectorAll(".text__first-bg"),
+    secBg = document.querySelectorAll(".text__second-bg"),
+    word = document.querySelectorAll(".text__word");
+
+  tl.to(firstBg, 0.2, { scaleX: 1 })
+    .to(secBg, 0.2, { scaleX: 1 })
+    .to(word, 0.1, { opacity: 1 }, "-=0.1")
+    .to(firstBg, 0.2, { scaleX: 0 })
+    .to(secBg, 0.2, { scaleX: 0 });
+};
