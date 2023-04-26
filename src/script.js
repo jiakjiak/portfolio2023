23e54d2b0edabd833852c78eab4dafa3fbd23b91
jiakjiak <88105937+jiakjiak@users.noreply.github.com>
2023-04-25 17:06:42 -0700

    Added scroll animation

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+//scroll animation//
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
 //navbar animation//
 const navbarScrolling = document.querySelector(".navbar");
 
